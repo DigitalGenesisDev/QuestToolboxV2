371e2bb92b80383c61be5945856e738c86f57448
mitchv2020 <70819540+mitchv2020@users.noreply.github.com>
2022-01-02 02:39:19 +0000

    adb files

--- QuestToolbox.py
+++ QuestToolbox.py
@@ -1,18 +1,26 @@
 import tkinter
-import customtkinter  # <- import the CustomTkinter module
+import customtkinter
 import platform
 
-root_tk = tkinter.Tk()  # create the Tk window like you normally do
+root_tk = tkinter.Tk()  # create the Tk window
 root_tk.geometry("1280x720")
 root_tk.title("QuestToolbox")
 
 os_name = platform.system()
 
-def button_function():
+def print_OSName():
     print(os_name)
 
 # Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=root_tk, corner_radius=10, command=button_function, text="Print OS Name")
-button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+test_button = customtkinter.CTkButton(master=root_tk, corner_radius=10, command=print_OSName, text="Print OS Name")
+test_button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 root_tk.mainloop()
+
+# Changes Recording Res and FPS to 1920x1080 60fps
+#
+# adb shell setprop debug.oculus.capture.width 1080
+# adb shell setprop debug.oculus.capture.height 1920
+# adb shell setprop debug.oculus.capture.bitrate 10000000
+# adb shell setprop debug.oculus.foveation.level 0
+# adb shell setprop debug.oculus.capture.fps 60
