52f81a2c74dc65cfce700c1a575a5e3bce6d7aaf
DJ <84008186+CADIndie@users.noreply.github.com>
2022-01-30 13:24:32 -0500

    Base Commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadFile('index.html')
+  }
