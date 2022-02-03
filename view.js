cb1ee002df6974255df629d36acdcddbaf7af85d
DJ <84008186+CADIndie@users.noreply.github.com>
2022-02-03 04:43:27 -0500

    Node Backend Work

--- view.js
+++ view.js
@@ -1,7 +1,7 @@
 let $ = require('jquery')  // jQuery now loaded and assigned to $
 let count = 0
-$('#click-counter').text(count.toString()
+$('#click-counter').text(count.toString())
 $('#countbtn').on('click', () => {
    count ++ 
    $('#click-counter').text(count)
-}) 
+})
