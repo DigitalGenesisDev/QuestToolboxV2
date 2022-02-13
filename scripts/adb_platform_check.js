9692188702b8d44186965296058bc3598b57a4b4
DJ <84008186+CADIndie@users.noreply.github.com>
2022-02-13 16:59:53 -0500

    IDFK

--- scripts/adb_platform_check.js
+++ scripts/adb_platform_check.js
@@ -5,4 +5,5 @@ const sysInfoBtn = document.getElementById('sys-info')
 sysInfoBtn.addEventListener('click', function () {
     const message = 'Your system ADB platform is set to: ${platform}'
     document.getElementById('got-sys-info').innerHTML = message
+    console.log(message)
  })
