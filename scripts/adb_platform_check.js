c2a8076a4288c54beae7572fa443faed78e3fc07
Gus Bunce <bunce627@gmail.com>
2022-04-08 17:23:04 +1000

    Clear out old code
    Removed old code to allow for the writing of new code

--- scripts/adb_platform_check.js
+++
@@ -1,9 +0,0 @@
-const os = require('os');
-const platform = os.platform()
-const sysInfoBtn = document.getElementById('got-sys-info')
-
-sysInfoBtn.addEventListener('click', function () {
-    const message = `Your system ADB platform is set to: ${platform}`
-    document.getElementById('got-sys-info').innerText = message
-    console.log(message)
- })
